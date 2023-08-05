4de188218be36b009c879a9f30bf7fae7cf8c9f2
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-08-04 20:53:19 -0600

    Add under_60 method to friends.rb

--- friends.rb
+++ friends.rb
@@ -1,6 +1,16 @@
-numbers = [3, 7, 17, 23, 56, 37, 99, 66]
+$numbers = [3, 7, 17, 23, 56, 37, 99, 66]
 
-numbers.each do |number|
-    puts number > 30 ? number : ""
+# $numbers.each do |number|
+#     puts number > 30 ? number : ""
  
+# end
+
+def under_60(arr)
+    $numbers.each do |number| 
+        if number < 60
+            puts number
+        end
+    end
 end
+
+under_60($numbers)
